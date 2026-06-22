ea6cb07fff7a13b4185a5912546a9edd35c3bc06
Sanchita Das <sanchitadasniki@gmail.com>
2026-06-22 22:15:12 +0530

    final project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+function App(){
+  const[project, setProject] = useState("");
+  const[tech, setTech] = useState("");
+  const[features, setFeatures] = useState("");
+  const[result, setResult] = useState("");
+
+  const generate = () => {
+    const output = `Developed ${project} using ${tech}, implementing ${features} to improve performance and user experience.;`
+    setResult(output);
+  };
+
+  return (
+    <div style={{maxWidth:"600px",margin:"auto",textAlign:"center",paddingTop:"50px"}}>
+    <h1>Resume Bullet Generator</h1>
+    <input placeholder="Project Name"onChange={(e) => setProject(e.target.value)}/><br /><br />
+    <input placeholder="Tech Stack"onChange={(e) => setTech(e.target.value)}/><br /><br />
+    <input placeholder="Features"onChange={(e) => setFeatures(e.target.value)}/><br /><br />
+
+    <button style={{padding:"10px",marginTop:"10px",cursor:"ponter"}}onClick={generate}>Generate</button>
+    <button onClick={() => navigator.clipboard.writeText(result)}>Copy Output</button>
+    <h3>Output:</h3><br />
+    <p>{result}</p>
+
+    <hr />
+
+    <a href="https://digitalheroesco.com" target="_blank">Built for Digital Heroes</a>
+    <p>Sanchita Das</p>
+    <p>Email:{"sanchitadasniki@gmail.com"}</p>
+    </div>
+    
+  );
+  }
+export default App;
+
+
